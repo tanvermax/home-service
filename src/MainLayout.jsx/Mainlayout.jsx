18c6c5c5e6813722a7a316349bf387a94f53bbf2
tanver909 <tanveermahidi@gmial.com>
2025-02-17 17:31:25 +0600

    fixes some error

--- src/MainLayout.jsx/Mainlayout.jsx
+++ src/MainLayout.jsx/Mainlayout.jsx
@@ -1,21 +1,35 @@
 import { Outlet } from "react-router-dom";
 import Nabver from "../Component/Navber/Nabver";
-import Home from "../Pages/Home/Home";
+
 import Fotter from "../Component/Fotter/Fotter";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import AuthContext from "../AuthProvider.jsx/AuhtContext";
 
+
 const Mainlayout = () => {
-  const {day} =useContext(AuthContext);
+  const { day } = useContext(AuthContext);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulating a data fetch
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
   return (
-    <div className={`${day ? "bg-black": "bg-white"}`}>
-   
-     <Nabver></Nabver>
- 
-      <div className=" min-h-[calc(100vh-300px)] scroll-x- ">
-      <Outlet></Outlet>
-      </div>
-      <Fotter></Fotter>
+    <div className="items-center text-center ">
+      {loading ? (
+        <span className="loading loading-bars mt-32 loading-3xl mx-auto w-20 "></span>
+      ) : (
+        <div className={`${day ? "bg-black" : "bg-white"}`}>
+          <Nabver></Nabver>
+
+          <div className=" min-h-[calc(100vh-300px)]  ">
+            <Outlet></Outlet>
+          </div>
+          <Fotter></Fotter>
+        </div>
+      )}
     </div>
   );
 };
